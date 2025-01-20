14a67fb5e8d8b5ea2e596d3b5fb75c7536e9b4a3
chloe tu <chloe_tu@outlook.com>
2025-01-20 00:47:43 -0500

    added comments

--- src/components/FB-intro/FB-intro.jsx
+++ src/components/FB-intro/FB-intro.jsx
@@ -7,19 +7,19 @@ import link_arrow from '../../assets/icons/group.svg'
 
 const Intro = () => {
     return (
-        <div className="FB-intro">
+        <div className="FB-intro"> // container for flex format
           <div className="FB-intro__content">
             <div className="FB-intro__icon">
               <img src={ai_symbol} alt="AI Symbol" />
             </div>
-            <div className="FB-intro__text">
+            <div className="FB-intro__text"> // left section
               <h2 className="FB-intro__text--heading">Flight Buddy, your AI flight assistant</h2>
               <p className="FB-intro__text--description">
                 We focus on what matters most: <br/> <span className="FB-intro__text--highlight">price, comfort, and time</span>. Let Flight Buddy prioritize your preferences for the best booking experience.
               </p>
             </div>
           </div>
-          <div className="FB-intro__features">
+          <div className="FB-intro__features"> // right section
             <h3 className="FB-intro__subheading">Book with confidence</h3>
             <ul className="FB-intro__list">
               <li className="FB-intro__list-item">
@@ -33,14 +33,13 @@ const Intro = () => {
               </li>
             </ul>
           </div>
-          <a
-      href="https://policies.google.com/privacy?hl=en#infocollectt" // Replace with your desired URL
-      target="_blank" // Opens link in a new tab
-      rel="noopener noreferrer"
-      className="FB-intro__privacy-link"
-    >
-      Learn More <img src={link_arrow} alt="Link to learn more" />
-    </a>
+            <a
+              href="https://policies.google.com/privacy?hl=en#infocollectt" 
+              target="_blank"
+              rel="noopener noreferrer"
+              className="FB-intro__privacy-link"
+                >Learn More <img src={link_arrow} alt="Link to learn more" />
+            </a> // opens a new tab to Google's privacy policy
         </div>
       );
     };
