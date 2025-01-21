30ed007b21b39737a01a2cc402280be221009d0f
i-am-lp <lp.phillips95@gmail.com>
2025-01-20 21:09:39 -0800

    updated merge conflicts in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TooltipBuddy from './components/Tooltip/Tooltip_flightBuddy'
+import TooltipHotelPicker from './components/ToolTip__hotelPicker/ToolTip__hotelPicker'
+import FlightBuddyPopUp from './components/FlightBuddyPopUp/flightBuddyPopUp'
 import FlightBuddyTab from './components/FlightBuddyTab/flightBuddyTab'
-import HomepageBody from './components/HomepageBody/homepage-body'
+import homePageBody from './components/HomepageBody/homepage-body'
+import FbIntro from './components/FB-intro/FB-intro'
+import Hotels from './components/Hotel/Hotels'
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-        < HomepageBody />
-        } />
+        <TooltipBuddy />   
+        <TooltipHotelPicker />
+        <FlightBuddyPopUp />
+        <FlightBuddyTab />
+        <homePageBody/>
+        <FbIntro/> 
+      
 
-        <Route path="/BOS-ORY" element={
-        < FlightBuddyTab />
-        } />
-    </Routes>
-    </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+
+export default App;
